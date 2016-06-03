409a2f7c914bbafb6f3913b849515d6150e2217a
Yellou <igormv@live.com>
2016-06-03 20:43:27 -0300

    Esboço mais detalhado do Detetive e Telas

--- Detetive/src/detetive/Detetive.java
+++ Detetive/src/detetive/Detetive.java
@@ -28,39 +28,62 @@ public class Detetive {
         
         ObjectInputStream in = new ObjectInputStream(det.server.getInputStream());
         ObjectOutputStream out = new ObjectOutputStream(det.server.getOutputStream()); //OUTPUT TEM QUE SER 1 PRA CADA PEER
+        
         try{
-            det.connectToServer(in);
+            det.connectToServer(in, out);
         }catch(IOException | ClassNotFoundException e){
             e.printStackTrace();
         }
         //conectado com o servidor
+        det.startGame(out);
+        
+        //COMO FAZER O FUNCIONAMENTO EM TURNOS
+//          while !jogoAcabou
+//              is my turn?
+//                  MEU TURNO (criar o palpite ou acusação e lançar pra galera) [makeGuess][makeAccusation]
+//                  for entre os outros jogadores pra checar se eles tem algo do palpite feito
+//                  if final da lista, volte pro inicio (ArrayList ciclico?)
+//                  se não incrementa posição da lista de sockets
+//                  passa o turno
 
     }
     
-    public void connectToServer(ObjectInputStream in) throws IOException, ClassNotFoundException{
+    public void connectToServer(ObjectInputStream in, ObjectOutputStream out) throws IOException, ClassNotFoundException{
         server = new Socket("127.0.0.1", 4321);
-        while(true){
+        while(in.readObject() != "Game Start"){
             peers = (ArrayList<Socket>) in.readObject();
-            //listar na tela os pares conectados
-            //identificar algum Start;
-            
+            //listar na tela os pares conectados USAR SWING
+//            if(/*Swing variavel*/){
+//                out.writeObject("Start");
+//            }
         }
     }
     
     public void startGame(ObjectOutputStream out){
-        //out.writeObject(START);  PRO SERVIDOR
+       //jogo rolando
+       /*
+       OU SERVIDOR, OU PLAYER QUE DEU START (ADICIONAR VARIAVEL PARA CHECAR ISSO -> PREFERENCIAL)
+       pegar do Guess a lista de pessoas, lugares e armas, embaralhar e dividir para os players
+       DEFINIR OS TURNOS (QUEM COMEÇAR, ORDEM, ETC.)
+       */
     }
     
     public void makeAccusation(ObjectOutputStream out) throws IOException{
         Guess guess = new Guess(Guess.Person.CoronelMostarda, Guess.Place.Biblioteca, Guess.Weapon.Cano);
-        out.writeObject(guess); //PARA TODOS OS PEERS
+        for(Socket client : peers){
+            ObjectOutputStream out1 = new ObjectOutputStream(client.getOutputStream());
+            out1.writeObject(guess);
+        }
         //Se acusação certa, jogo termina
         //Se acusação errada, jogador sai do jogo
     }
     
     public void makeGuess(ObjectOutputStream out) throws IOException{
         Guess guess = new Guess(Guess.Person.CoronelMostarda, Guess.Place.Biblioteca, Guess.Weapon.Cano);
-        out.writeObject(guess); //PARA TODOS OS PEERS
+        for(Socket client : peers){
+            ObjectOutputStream out1 = new ObjectOutputStream(client.getOutputStream());
+            out1.writeObject(guess);
+        }
     }
     
 }
